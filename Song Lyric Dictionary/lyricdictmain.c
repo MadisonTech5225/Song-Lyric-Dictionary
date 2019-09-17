6cec02397c0e53ea32031494865d9a1c2efca574
madisontech5225 <madisontech5225@gmail.com>
2019-09-17 11:21:07 -0400

    Initial commit

---
+++ Song Lyric Dictionary/lyricdictmain.c
@@ -0,0 +1,17 @@
+/*
+    lyricdictmain.c
+    Song Lyric Dictionary
+
+    Created by Kymn, Stephen on 9/17/19.
+    Copyright © 2019 Kymn, Stephen. All rights reserved.
+ 
+    Abstract: Main logic for the program.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World!\n");
+    return 0;
+}
